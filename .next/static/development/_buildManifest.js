ee8e4879682e79b38304e0889ee7ab08f3461677
Mac Decker <mcdecke@gmail.com>
2022-02-25 11:41:08 -0700

    deleting node and next

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/monsters/monsterView":["static\u002Fchunks\u002Fpages\u002Fmonsters\u002FmonsterView.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fmonsters\u002FmonsterView"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
